eb844a5207ab8aa5b31877df7cf92c810760975d
Jonathan Cockrell <cockrellj99@lsus.edu>
2025-02-24 16:21:15 -0600

    set a static root to try to resolve the heroku problem

--- myproject/settings.py
+++ myproject/settings.py
@@ -81,6 +81,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 #STATICFILES_DIRS = [
 #    BASE_DIR / 'core' / 'js',  # This tells Django to also look in the core/js directory.
